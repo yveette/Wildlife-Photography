b4374c6c412069a79e2246c238ba5b033a31f587
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-12 16:27:51 +0200

    Connect to database

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// TODO change database name
+const dbName = 'wildlife';
+const connnectionString = `mongodb://localhost:27017/${dbName}`;
+
+module.exports = async (app) => {
+    try {
+        mongoose.connect(connnectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('Database connected');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('Database error');
+            console.error(err);
+        });
+    } catch (err) {
+        console.error('Error connecting to database');
+        process.exit(1);
+    }
+
+}
