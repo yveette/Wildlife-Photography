c1c4517294fe293bf0ecb1eab8eb4c2fc961a0a5
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-14 19:39:32 +0200

    Create post model, service and validation

---
+++ services/post.js
@@ -0,0 +1,17 @@
+const Post = require('../models/Post');
+
+async function createPost(post) {
+    const result = new Post(post);
+    await result.save();
+
+    return result;
+}
+
+async function getPosts() {
+    return Post.find({});
+}
+
+module.exports = {
+    createPost,
+    getPosts
+}
