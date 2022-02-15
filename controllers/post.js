047f43dd5ff91292d7300dc82e0540316eddf898
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-15 15:25:49 +0200

    Implemented edit post functionality

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { isUser } = require('../middleware/guards');
-const { createPost } = require('../services/post');
-const { mapErrors } = require('../util/mappers');
+const { createPost, getPostById, updatePost } = require('../services/post');
+const { mapErrors, postViewModel } = require('../util/mappers');
 
 router.get('/create', isUser(), (req, res) => {
     res.render('create', { title: 'Create Post' });
@@ -26,6 +26,39 @@ router.post('/create', isUser(), async (req, res) => {
         console.error(err);
         const errors = mapErrors(err);
         res.render('create', { title: 'Create Post', errors, data: post });
+    }
+});
+
+router.get('/edit/:id', isUser(), async (req, res) => {
+    const id = req.params.id;
+    const post = postViewModel(await getPostById(id));
+
+    if (req.session.user._id != post.author._id) {
+        res.redirect('/login');
+    }
+
+    res.render('edit', { title: 'Edit Post', post });
+});
+
+router.post('/edit/:id', isUser(), async (req, res) => {
+    const id = req.params.id;
+    const post = {
+        title: req.body.title,
+        keyword: req.body.keyword,
+        location: req.body.location,
+        date: req.body.date,
+        image: req.body.image,
+        description: req.body.description,
+    };
+
+    try {
+        await updatePost(id, post);
+        res.redirect('/catalog/' + id);
+    } catch (err) {
+        console.error(err);
+        const errors = mapErrors(err);
+        post._id = id;
+        res.render('edit', { title: 'Edit Post', errors, post });
     }
 });
 
--- services/post.js
+++ services/post.js
@@ -16,8 +16,22 @@ async function getPostById(id) {
     return Post.findById(id).populate('author', 'firstName lastName');
 }
 
+async function updatePost(id, post) {
+    const existing = await Post.findById(id);
+
+    existing.title = post.title;
+    existing.keyword = post.keyword;
+    existing.location = post.location;
+    existing.date = post.date;
+    existing.image = post.image;
+    existing.description = post.description;
+
+    await existing.save();
+}
+
 module.exports = {
     createPost,
     getPosts,
-    getPostById
+    getPostById,
+    updatePost
 }
